b36494853831b57440123b56672ddb781b808d23
PealHassan <pealhasan6@gmail.com>
2024-06-16 23:06:41 +0600

    upgrade 12

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // require('dotenv').config();
-var mongoUrl = 'mongodb+srv://pealhassan:kvzoszhhzm@hotelmanagement.j8lqtgl.mongodb.net/Rooms';
+var mongoUrl = 'mongodb+srv://pealhassan:kvzoszhhzm@hotelmanagement.j8lqtgl.mongodb.net/?retryWrites=true&w=majority&appName=HotelManagement/Rooms';
 // var mongoUrl = process.env.MONGO_URI;
 
 mongoose.connect(mongoUrl);
