0a4ba92376fe5d736b92cb1d17b2c7cc05b48a36
PealHassan <pealhasan6@gmail.com>
2024-06-16 22:40:53 +0600

    upgrade 2

--- server/server.js
+++ server/server.js
@@ -1,40 +1,40 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const app = express();  
-const dbconfig = require('./db');
-const usersRoute = require('./routes/userRoute');
-const roomsRoute = require('./routes/roomRoute');
-const bookRoute = require('./routes/bookingRoute');
-const packageRoute = require('./routes/packageRoute');
-const ticketbookRoute = require('./routes/ticketbookingRoute');
+// const dbconfig = require('./db');
+// const usersRoute = require('./routes/userRoute');
+// const roomsRoute = require('./routes/roomRoute');
+// const bookRoute = require('./routes/bookingRoute');
+// const packageRoute = require('./routes/packageRoute');
+// const ticketbookRoute = require('./routes/ticketbookingRoute');
 
-const cors = require('cors');
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-app.use(express.json());
+// const cors = require('cors');
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }));
+// app.use(express.json());
 app.use('/',(req,res) => {
   res.send("server is running");
 })
 // require('dotenv').config();
 
-app.use('/api/rooms', roomsRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/booking', bookRoute);
-app.use('/api/packages', packageRoute);
-app.use('/api/ticketbook', ticketbookRoute);
+// app.use('/api/rooms', roomsRoute);
+// app.use('/api/users', usersRoute);
+// app.use('/api/booking', bookRoute);
+// app.use('/api/packages', packageRoute);
+// app.use('/api/ticketbook', ticketbookRoute);
 
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 
 
 app.listen(port, () => console.log(`Node server started on port ${port}`));
-module.exports = app; 
+// module.exports = app; 
 
